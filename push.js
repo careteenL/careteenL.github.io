a0b12b0fe01c15c8b82e354d80f99c7dccaaf4f3
ketingwang <ketingwang213821@sohu-inc.com>
2018-02-27 11:35:35 +0800

    modifiedPush.js

--- push.js
+++ push.js
@@ -3,9 +3,9 @@ require('shelljs/global');
 var comment = process.argv[2]
 
 if (!comment) {
-    console.log(`输入注释： node push.js 'add page'`);
+    console.log(`输入注释： node push.js addPage`);
     return
 }
 exec(`git add .`);
-exec(`git commit -m ${comment}`);
+exec(`git commit -m '${comment}'`);
 exec(`git push`);
